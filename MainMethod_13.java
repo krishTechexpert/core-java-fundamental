b954f8ba51ef3ae963edcead29ad48053c5caceb
krish <krishcode9@gmail.com>
2025-01-17 12:55:43 +0530

    concept of main method

---
+++ MainMethod_13.java
@@ -0,0 +1,15 @@
+class MainMethod_13{
+  static public void main(String... krishna){
+    System.out.println("main valid method");//working well
+  }
+
+  //MainMethod_13.java:5: warning: [strictfp] as of release 17, all floating-point expressions are evaluated strictly and 'strictfp' is not required
+  // static final synchronized strictfp public void main(String... krishna){
+  //   System.out.println("it is main valid method");
+  // }
+
+  // main method can be overloading but jvm will call main(String[] args) this method.
+
+  
+
+}
