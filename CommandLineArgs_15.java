86d23eb0d7d6174ead5ce0d93d617a54b6b15702
krish <krishcode9@gmail.com>
2025-01-19 15:57:59 +0530

    section 16: Coding standard complete

---
+++ CommandLineArgs_15.java
@@ -0,0 +1,35 @@
+class CommandLineArgs_15{
+
+  // example-1
+  // public static void main(String... krishna){
+  //   int sum=0;
+  //   for(int i=0;i<krishna.length;i++){
+  //       int n = Integer.parseInt(krishna[i]);//convert string into integer 6,10,30
+  //     sum+=n;
+  //   }
+  //   System.out.println(sum); //46
+  // }
+//example-2
+//internally jym create like that String[] arr = {'6','10','30'} which will be passed into main Method as command line args inside String[] args so args contains array of string such as arr
+
+  public static void main(String[] args){
+    int sum=0;
+    for(int i=0;i<args.length;i++){
+        int n = Integer.parseInt(args[i]);//convert string into integer 6,10,30
+      sum+=n;
+    }
+    System.out.println(sum); //46
+  }
+
+
+  // public static void main(String[] args){
+  //   int sum=0;
+  //   String[] arr = {"10","20","30"};
+    
+  //   for(int i=0;i<arr.length;i++){
+  //       int n = Integer.parseInt(arr[i]);//convert string into integer 10,20,30
+  //     sum+=n;
+  //   }
+  //   System.out.println(sum); //60
+  // }
+}
---
+++ JavaCodingStandard_16.java
@@ -0,0 +1,55 @@
+class JavaCodingStandard_16{
+  // public int m1(int x,int y){
+  //   return x+y // refactor code with meaningfull name
+  // }
+  public static int add(int number1,int number2){
+    return number1 + number2;
+  }
+}
+
+//1.) first define package like format package com.durgasoft.auth(module name)
+//2.) class name always public with pasacl case with meaning full name
+//3.) m1 is instance method but we are not using any instance varaible insdie m1 so in that case
+// public int m1(int x,int y){
+//     return x+y // local varaible
+//   }
+// so in that case make it static such as
+// public static int m1(int x,int y) {
+//   return x+y;//// local varaible
+// }
+
+
+//coding standard for Interface start with uppercacse letter and second word also uppercase
+//interface such as Runnable,Seraiblable,Comparable,RandomAccess
+
+
+//coding standard for method such as camelcase
+// single word start with lower case like print(),sleep(),run(),eat(),star(),main(),
+//combineWord such as secondWord should be start with uppercase
+//getName(),setSalary()
+
+//for Varaible name coding standard , age, salary, mobileNumber
+
+
+//constant standard name  such as MAX_VALUE,MAX_PRIORITY,
+
+
+// public class StudentBean {
+//   private String name;
+//   public  void setName(String name){
+//     this.name=name;
+//   }
+//   public String getName(){
+//     return name;
+//   }
+// }
+
+
+//coding standard for method with parameter a is correct format
+
+//a.) public void addMyActionListener(myActionListener l)(correct)
+      //public void removeMyActionListener(myActionListenerl)(correct)
+
+//b.) public void registerMyActionListener(myActionListener l) (wrong)
+
+//c.) Public void addMyActionListener(ActionListener l) (wrong)
