7d16d0fdb236cbc66602a4953976758f6624ea93
krish <krishcode9@gmail.com>
2025-03-04 18:54:48 +0530

    spring core theory done Imp

--- SpringCore/SprintBasic/src/main/java/IOCContainer.java
+++ SpringCore/SprintBasic/src/main/java/IOCContainer.java
@@ -35,5 +35,39 @@
 
 /* --------- How to make Spring Application -----------*/
 
+/* Pre-requisites: java,IDE, Dependency
+    1.create maven project
+*   2. Add spring context dependency in pom.xml file
+        such as
+        <!-- https://mvnrepository.com/artifact/org.springframework/spring-context -->
+        <dependency>
+            <groupId>org.springframework</groupId>
+            <artifactId>spring-context</artifactId>
+            <version>6.2.3</version>
+        </dependency>
+
+    spring application k pass ek IOC hota hai,Jo configuration context ko read kerta hai
+
+    3. create required java classes
+    4. create bean configuration file and bean defination such as
+                    <?xml version="1.0" encoding="UTF-8"?>
+        <beans xmlns="http://www.springframework.org/schema/beans"
+            xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+            xsi:schemaLocation="
+                http://www.springframework.org/schema/beans http://www.springframework.org/schema/beans/spring-beans.xsd">
+
+            <!-- bean definitions here -->
+
+        </beans>
+    5. create main class and start IOC container such as
+        ApplicationContext ap=new new ClassPathXmlApplicationContext("bean.xml"); //IOC reads bean.xml, creates beans, and injects dependencies.
+
+        Car car = ap.getBean(Car.class)
+* */
+
+
+
+
+
 public class IOCContainer {
 }
