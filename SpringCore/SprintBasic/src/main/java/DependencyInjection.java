584750490b227fac338e363a027c688c55a38064
krish <krishcode9@gmail.com>
2025-03-04 13:46:04 +0530

    Spring core all theory and basic concept finished ..this is bery imp

--- SpringCore/SprintBasic/src/main/java/DependencyInjection.java
+++ SpringCore/SprintBasic/src/main/java/DependencyInjection.java
@@ -1,2 +1,17 @@
+//-------- What is Dependency Injection (DI)
+/*
+* The process of injecting one class object into another object is called DI.
+* In other words,
+* Dependency Injection (DI) is a design pattern used in Java to reduce tight coupling
+* between classes. It allows one class to receive its dependencies from an external
+* source rather than creating them itself.
+
+*
+ * we can perform DI in 3 ways:
+*   a.) setter injection
+*   b.) constructor injection
+*   c.) field injection
+* */
+
 public class DependencyInjection {
 }
