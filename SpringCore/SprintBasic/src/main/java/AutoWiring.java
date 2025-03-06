fc0366d9bbb5c484cdc816ca11e1c8283001c4fc
krish <krishcode9@gmail.com>
2025-03-06 15:43:11 +0530

    update spring basic theory

--- SpringCore/SprintBasic/src/main/java/AutoWiring.java
+++ SpringCore/SprintBasic/src/main/java/AutoWiring.java
@@ -3,6 +3,7 @@
 /*
 * we can inject object bean into target in 2 ways
 * 1.) Manual wiring:  such as we defined bean in bean.xml is called manual wiring
+*   such as propertyTag we used, ref attribute and constructor-arg we already used
 * 2.) Autowiring: it means IOC container will identify dependent bean and it will inject
 * into target bean ( we no need to use any ref attribute in bean configuration file)
 *   it will work based on below modes
@@ -14,7 +15,50 @@
 * Note: Autowiring will not work by default, we have to enable it
 *       <bean id="car" class="org.example.Car" scope="prototype" auto-wire="byName | byType">
 *
-* */
+* no need to ref attribute and property tag to inject bean then used autowiring by name and by type etc.
+* -------------------- byName-----------------
+* byName: it means IOC will identify dependent bean object based on bean id or bean name
+* <bean id="engine" class="org.example.Kseries" />//engine used ho reha hai byName
+* yha per engine represent instance varaible  hai for private Engine engine;
+*
+* <bean id="engine1" class="org.example.Mhawkseries" />
+*
+* <bean id="car" class="org.example.Car" scope="prototype" auto-wire="byName"/>
+*
+* ------------------byType--------------
+* byType: it means IOC identify dependent bean object based on data
+*           type of the varaible in target class
+*
+*   <bean id="engine1(anyName)" class="org.example.Kseries" />
+* * <bean id="engine2" class="org.example.Mhawkseries" /> it will give ambugity error
+* because two object engine1,engine2 is of same type Engine . check solution
+
+ * <bean id="car" class="org.example.Car" scope="prototype" auto-wire="byType"/>
+ *
+ * -------- Solution 1 (autowire-candidate="false") ----------
+ *   <bean id="engine1" class="org.example.Kseries" />
+ * * <bean id="engine2" class="org.example.Mhawkseries" autowire-candidate="false" />
+ *
+ * 🔹 Why Use autowire-candidate="false"?
+It prevents Spring from considering the bean for autowiring.(engine2 will not participate in autowiring)
+Even if there are matching types, Spring will ignore this bean.
+Useful when you have multiple beans of the same type but only want some to be
+* injected automatically.
+
+ *
+ *  * <bean id="car" class="org.example.Car" scope="prototype" auto-wire="byType"/>
+
+*
+* -------- Solution 2 (primary="true") ----------
+ *   <bean id="engine1" class="org.example.Kseries" primary="true" />
+ * * <bean id="engine2" class="org.example.Mhawkseries" />
+ * *
+ *  * <bean id="car" class="org.example.Car" scope="prototype" auto-wire="byType"/>
+*
+* now engine1 take priority and engine2 will ignored
+ * */
+
+
 
 public class AutoWiring {
 }
---
+++ SpringCore/SprintBasic/src/main/java/InterviewQuestion.java
@@ -0,0 +1,18 @@
+/*
+* -------- Spring Important Interview question----------
+*   1.) spring bean life cycle
+*   2.) what is spring bean
+*   3.) how to represent java class as spring bean
+*   4.) what is IOC container and type of IOC container (IMP)
+*   5.) how to start IOC container
+*   6.) what type of DI and explain each (IMP)
+*   7.) lazy loading(Bean factory) vs eager loading (Application Context)
+*   8.) what is bean scope and it's type explain any 4 (IMP)
+*   9.) what is Autowiring and it's mode
+*
+*
+* */
+
+
+public class InterviewQuestion {
+}
