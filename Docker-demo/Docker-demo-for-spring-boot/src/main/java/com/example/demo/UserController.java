4d861203b6be3ecc2889986cdac7c28512a60180
krish <krishcode9@gmail.com>
2025-04-19 12:43:37 +0530

    docker complete with spring boot basic app with mysql

---
+++ Docker-demo/Docker-demo-for-spring-boot/src/main/java/com/example/demo/UserController.java
@@ -0,0 +1,23 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    @Autowired
+    private UserRepo userRepo;
+
+    @PostMapping
+    public User addUser(@RequestBody User user){
+       return userRepo.save(user);
+    }
+
+    @GetMapping
+    public List<User> getAllUser(){
+        return userRepo.findAll();
+    }
+}
